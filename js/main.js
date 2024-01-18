feb5f306eb7b0a7135d0db0ac9d7f019af4a06e9
Klaudiusz <klaudecki@gmail.com>
2024-01-18 21:47:23 +0100

    added input table

--- js/main.js
+++ js/main.js
@@ -15,8 +15,10 @@ const peopleError = document.querySelector('.error-people')
 const buttons = document.querySelectorAll('.tip-btn')
 const btnReset = document.querySelector('.reset')
 
-//Error Check
+// Table input
+const inputs = [billInput, peopleInput, customInput]
 
+//Error Check
 const checkBillError = () => {
 	if (billInput.value == '' || billInput.value < 0) {
 		billError.style.display = 'block'
@@ -27,40 +29,22 @@ const checkBillError = () => {
 	}
 }
 
-const countBill = () => {
-	const price = billInput.valueAsNumber
-	const people = peopleInput.valueAsNumber
-	const tip = (customInput.value / 100) * billInput.value
-
-	if ((billInput.value !== '') && (peopleInput.value !== '') && (peopleInput.value > 0)) {
-		const sum = ((price + tip) / people).toFixed(2)
-
-		const tipSum = (tip / people).toFixed(2)
-
-		totalCost.textContent = `$` + `${sum}`
-		tipCost.textContent = `$` + `${tipSum}`
-		peopleError.style.display = 'none'
-		billError.style.display = 'none'
-	} else {
-		checkBillError()
-	}
-}
-
+// Maths
 buttons.forEach(button => {
 	button.addEventListener('click', e => {
-		const tipValue = parseInt(e.target.data)
+		const tipValue = parseInt(e.target.dataset.set)
 
 		const price = billInput.valueAsNumber
 		const people = peopleInput.valueAsNumber
 
 		const valuesBtn = (tipValue / 100) * billInput.value
 
-		if ((billInput.value !== '') && (peopleInput.value !== '') && (peopleInput.value > 0)) {
+		if (billInput.value !== '' && peopleInput.value !== '' && peopleInput.value > 0) {
 			const sum = ((price + valuesBtn) / people).toFixed(2)
 
 			const tipSum = (valuesBtn / people).toFixed(2)
-			totalCost.textContent = `$` + `${sum}`
-			tipCost.textContent = `$` + `${tipSum}`
+			totalCost.textContent = `$${sum}`
+			tipCost.textContent = `$${tipSum}`
 			peopleError.style.display = 'none'
 			billError.style.display = 'none'
 			customInput.value = ''
@@ -70,9 +54,29 @@ buttons.forEach(button => {
 	})
 })
 
-// Reset
+inputs.forEach(input => {
+	input.addEventListener('input', () => {
+		const price = billInput.valueAsNumber
+		const people = peopleInput.valueAsNumber
+		const tip = (customInput.value / 100) * billInput.value
 
-const reset = () => {
+		if (billInput.value !== '' && peopleInput.value !== '' && peopleInput.value > 0) {
+			const sum = ((price + tip) / people).toFixed(2)
+
+			const tipSum = (tip / people).toFixed(2)
+
+			totalCost.textContent = `$${sum}`
+			tipCost.textContent = `$${tipSum}`
+			peopleError.style.display = 'none'
+			billError.style.display = 'none'
+		} else {
+			checkBillError()
+		}
+	})
+})
+
+// Reset
+btnReset.addEventListener('click', () => {
 	billInput.value = ''
 	peopleInput.value = ''
 	customInput.value = ''
@@ -80,9 +84,4 @@ const reset = () => {
 	tipCost.textContent = '$0.00'
 	peopleError.style.display = 'none'
 	billError.style.display = 'none'
-}
-
-btnReset.addEventListener('click', reset)
-billInput.addEventListener('input', countBill)
-peopleInput.addEventListener('input', countBill)
-customInput.addEventListener('input', countBill)
+})
